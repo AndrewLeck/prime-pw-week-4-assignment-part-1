e2421d8bedff314098a1080dd4d8ce624e08f990
Andrew Leck <Aleck360@gmail.com>
2022-04-30 20:39:32 -0500

    fixed questions 2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,17 +14,16 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return; 
-}
+//function helloName( name ) {
+  //return 
+//}
 
 function helloName(name){
-   name = 'Hello, Andrew!'; 
   return name;
 
 }
 // Remember to call the function to test
-console.log( helloName() ) ;
+console.log(helloName('Hello, Breanna')) ;
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
